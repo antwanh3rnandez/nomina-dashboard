67c84d0c8e4402b26d0d21c7fffab225133e982a
antwanh3rnandez <115040575+antwanh3rnandez@users.noreply.github.com>
2024-03-05 23:15:27 -0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import { poppins } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={`${poppins.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>Nomina Dashboard by ❤️ Gethy</footer>
+      </body>
     </html>
   );
 }
